7a82c64243ef856dc1d78c7b29e43079bb12afd5
HaZ3R0 <megamackan@outlook.com>
2024-04-03 10:41:18 +0200

    Add project files.

---
+++ Klasser uppgift1/Program.cs
@@ -0,0 +1,18 @@
+﻿using Klasser_uppgift1;
+using System;
+namespace klasserUppgift1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bil p = new Bil ("Bmw", 1974, "blå", 6000, 4);
+
+            Console.WriteLine(p.märke);
+            Console.WriteLine(p.årsmodell);
+            Console.WriteLine(p.färg);
+            Console.WriteLine(p.mil);
+            Console.WriteLine(p.däck);
+        }
+    }
+}
---
+++ Klasser uppgift1/klasserUppgift1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Klasser_uppgift1
+{
+    class Bil
+    {
+        public string? märke;
+        public int? årsmodell;
+        public string? färg;
+        public int? mil;
+        public int? däck;
+
+        public Bil(string märke2, int årsmodell2, string färg2, int mil2, int däck2)
+        {
+            märke = märke2;
+            årsmodell = årsmodell2;
+            färg = färg2;
+            mil = mil2;
+            däck = däck2;
+        }
+    }
+}
